a9b7a28e2a0406b3b24858ffb3625bfb1dda0051
neokarn <34811101+neokarn@users.noreply.github.com>
2019-02-23 18:39:56 +0700

    Create lab13_2.cpp

---
+++ lab13_2.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+const int N = 5;
+
+void inputMatrix(double [][N]);
+
+void findLocalMax(const double [][N], bool [][N]);
+
+void showMatrix(const bool [][N]);
+
+int main(){
+	double A[N][N]; 
+	bool B[N][N]; 
+	cout << "Input the matrix.....\n";
+	inputMatrix(A);
+	findLocalMax(A,B);
+	cout << "Result is \n";
+	showMatrix(B);
+	return 0;
+}
+
+// Write definition of inputMatrix(),matrixMultiply() and showMatrix() here
