afda39b935cab442178e7a7fad8292a3bf1c3e7a
neokarn <34811101+neokarn@users.noreply.github.com>
2019-02-23 19:05:30 +0700

    Create lab13_1.cpp

---
+++ lab13_1.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+template <typename T>
+void swap(T d[],int x,int y){
+    T temp = d[x];
+    d[x] = d[y];
+    d[y] = temp;
+}
+
+template <typename T>
+void insertionSort(T d[],int N){
+	
+//Write your code here.
+
+}
+
+int main(){
+	int a[10] = {12,25,30,44,2,0,4,7,55,25};
+	cout << "Input Array:";
+	for(int i = 0; i < 10; i++) cout << a[i] << " ";
+	cout << "\n\n";
+	
+	insertionSort(a,10);
+	
+	cout << "\nSorted Array:";
+	for(int i = 0; i < 10; i++) cout << a[i] << " ";	
+}
