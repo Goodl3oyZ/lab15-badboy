b89fc0da28b34fad7599ef25d4a5998a94a610b0
Goodl3oyZ <panudetzx2@gmail.com>
2023-02-07 13:45:30 +0700

    lab 15-2 sed nanla

--- lab15_2.cpp
+++ lab15_2.cpp
@@ -19,3 +19,50 @@ int main(){
 	showMatrix(B);
 	return 0;
 }
+
+void inputMatrix(double A[][N]){
+	for(int i=0;i<N;i++){
+		cout << "Row " <<i+1<<": ";
+		for(int j=0;j<N;j++){
+
+		cin >> A[i][j] ;
+		}
+	}
+}
+
+void findLocalMax(const double A[N][N], bool B[N][N]){
+	for(int i = 1; i < N-1 ; i++){
+		for(int j = 1; j < N-1 ; j++){
+			if(A[i][j] >= A[i-1][j] and A[i][j] >= A[i][j-1] and A[i][j] >= A[i][j+1] and A[i][j] >= A[i+1][j]){
+				B[i][j] = 1;
+			}else{
+				B[i][j] = 0;
+			}
+		}
+	}
+	for(int i=0; i<N; i++){
+		for(int j=0; j<N; j++){
+			if(i == 0){
+				B[i][j] = 0;
+			}
+			if(i == N-1){
+				B[i][j] = 0;
+			}
+			if(j == 0){
+				B[i][j] = 0;
+			}
+			if(j == N-1){
+				B[i][j] = 0;
+			}
+		}
+	}
+}
+
+void showMatrix(const bool A[][N]){
+for(int i=0;i < N;i++){
+    for(int j=0; j < N ;j++){
+    cout << A[i][j] << " ";
+    }
+    cout << "\n" ;
+    }
+    }
